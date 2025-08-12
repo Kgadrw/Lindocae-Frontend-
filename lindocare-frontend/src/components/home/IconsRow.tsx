6702f1b10c9c88f112b48c1e32f643ced055bd90
Kgad <kalisagad05@gmail.com>
2025-08-12 19:01:30 +0200

    changed icons styles1

--- lindocare-frontend/src/components/home/IconsRow.tsx
+++ lindocare-frontend/src/components/home/IconsRow.tsx
@@ -133,12 +133,11 @@ const IconsRow: React.FC<IconsRowProps> = ({ icons, iconsLoading, iconsError }) 
       ) : (
         <div className="w-full">
           {/* Auto-sliding container */}
-                  <div 
-          ref={scrollContainerRef}
-          className="flex gap-5 pb-4 justify-center flex-wrap"
-          style={{ overflowX: 'hidden' }}
-        >
-
+          <div 
+            ref={scrollContainerRef}
+            className="flex gap-5 overflow-x-hidden justify-center pb-4"
+            style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
+          >
             {icons?.map((icon: Icon, idx: number) => {
               let image = '';
               if (Array.isArray(icon.image) && icon.image.length > 0) image = icon.image[0];
