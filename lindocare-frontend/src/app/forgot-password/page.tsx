e914a044fee1fadcb7acc482f3ff47e3933a0ab4
Kgad <kalisagad05@gmail.com>
2025-08-13 19:41:44 +0200

    changed forget password , searching params1

--- lindocare-frontend/src/app/forgot-password/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'next/navigation';
-import ResetPassword from '../../components/Reset-password';
-
-export default function ResetPasswordPathPage() {
-  const params = useParams();
-  const token = params.token as string | undefined;
-
-  return <ResetPassword token={token || null} />;
-}
